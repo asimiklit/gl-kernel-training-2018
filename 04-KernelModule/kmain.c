9244efc9f68a139ced14bc32e138460960006ad6
Andrii Simiklit <asimiklit.work@gmail.com>
2018-12-11 21:06:58 +0000

    04: added a parameter to module just to test how to use it
    Signed-off-by: Andrii Simiklit <asimiklit.work@gmail.com>

--- 04-KernelModule/kmain.c
+++ 04-KernelModule/kmain.c
@@ -7,14 +7,16 @@ MODULE_AUTHOR("Andrii Simiklit");
 MODULE_DESCRIPTION("My simple driver test");
 MODULE_VERSION("0.1");
 
+static int myparam = -1;
+module_param(myparam, int, 0);
 
 static int __init hello_init(void) {
-   printk(KERN_DEBUG "Hello everyone!!!!");
-   return 0;
+   printk(KERN_DEBUG "Hello everyone!!!! myparam(%d)", myparam);
+   return myparam;
 }
 
 static void __exit hello_exit(void) {
-   printk(KERN_DEBUG "My own driver test finished!");
+   printk(KERN_DEBUG "My own driver test finished! myparam(%d)", myparam);
 }
 
 module_init(hello_init);
