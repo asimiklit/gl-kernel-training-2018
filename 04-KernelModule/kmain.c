270b4186e52ab68af4c3bd289934ce8fabf6586b
Andrii Simiklit <asimiklit.work@gmail.com>
2018-12-11 19:14:47 +0000

    04-kernel_module: Fix many compilation errors
    v1:
    1. makefile -> Makefile
    2. missing modules.h inclusion
    3. parameter list of functions are replaced
       from  () to (void) (mandatory) I didn't know it
    4. makros KERNEL_DEBUG -> KERN_DEBUG
    5. MODULE_DESCRPTION -> MODULE_DESCRIPTION
    
    Signed-off-by: Andrii Simiklit <asimiklit.work@gmail.com>

--- 04-KernelModule/kmain.c
+++ 04-KernelModule/kmain.c
@@ -1,20 +1,20 @@
 #include <linux/init.h>
-#include <linux/init.h>
+#include <linux/module.h>
 #include <linux/kernel.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrii Simiklit");
-MODULE_DESCRPTION("My simple driver test");
+MODULE_DESCRIPTION("My simple driver test");
 MODULE_VERSION("0.1");
 
 
-static int __init hello_init() {
-   printk(KERNEL_DEBUG "Hello everyone!!!!");
+static int __init hello_init(void) {
+   printk(KERN_DEBUG "Hello everyone!!!!");
    return 0;
 }
 
-static void __exit hello_exit() {
-   printk(KERNEL_DEBUG "My own driver test finished!");
+static void __exit hello_exit(void) {
+   printk(KERN_DEBUG "My own driver test finished!");
 }
 
 module_init(hello_init);
